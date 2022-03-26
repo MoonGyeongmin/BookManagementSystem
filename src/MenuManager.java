42f85f9a2452fc7763c95834a495c33e44e9d792
MoonGyeongmin <ansrudals5828@gmail.com>
2022-03-26 22:20:16 +0900

    Edit with Methods

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class MenuManager {
-	
+
 	public static void main(String[] args) {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
-		
-		
+
 		while (num != 6) {
 			System.out.println("1. Add Books");
 			System.out.println("2. Delete Books");
@@ -15,38 +14,55 @@ public class MenuManager {
 			System.out.println("5. Show a Menu");
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6:");
-			
-			num = input.nextInt();
-			
-			if (num == 1) {
-				System.out.print("Book Code: ");
-				int bookCode = input.nextInt();
-				input.nextLine();
-				
-				System.out.print("Book Name: ");
-				String bookName = input.nextLine();
 
-				
-				System.out.print("Author Name: ");
-				String authorName = input.nextLine();
-			
-				System.out.print("Publisher Name: ");
-				String publisherName = input.nextLine();
+			num = input.nextInt();
+
+			if (num == 1) {
+				addBooks();
+
 			}
 			else if (num == 2) {
-				System.out.print("Book Code: ");
-				int bookCode2 = input.nextInt();
+				deleteBooks();
 			}
 			else if (num == 3) {
-				System.out.print("Book Code: ");
-				int bookCode2 = input.nextInt();
+				editBooks();
 			}
 			else if (num == 4) {
-				System.out.print("Book Code: ");
-				int bookCode2 = input.nextInt();
+				viewBooks();
 			}
-			
-			
 		}
 	}
+
+	public static void addBooks() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Book Code: ");
+		int bookCode = input.nextInt(); 
+		input.nextLine();
+
+		System.out.print("Book Name: ");
+		String bookName = input.nextLine();
+
+		System.out.print("Author Name: ");
+		String authorName = input.nextLine();
+
+		System.out.print("Publisher Name: ");
+		String publisherName = input.nextLine();	
+	}
+	public static void deleteBooks() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Book Code: ");
+		int bookCode = input.nextInt();
+	}
+	public static void editBooks() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Book Code: ");
+		int bookCode = input.nextInt();
+	}
+	public static void viewBooks() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Book Code: ");
+		int bookCode = input.nextInt();	
+	}
+
+
 }
